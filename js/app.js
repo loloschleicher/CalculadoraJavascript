068ffbca200f30d36b96adc721e2c53cfd930122
Leonel Schleicher <lolo042@hotmail.com>
2020-01-22 11:46:52 -0300

    Correciones

--- js/app.js
+++ js/app.js
@@ -1,279 +1,196 @@
+var calculadora = {
+	
+	visor: document.getElementById("display"),
+	valorVisor: "0",
+	operacion: "",
+	primerValor: 0,
+	segundoValor: 0,
+	ultimoValor: 0,
+	resultado: 0,
+	auxTeclaIgual: false,
+	
+	init: (function(){
+		this.asignarEventosFormatoBotones(".tecla");
+		this.asignarEventosaFuncion();
+	}),
+	
+	//Eventos de formato de botones
+	
+	asignarEventosFormatoBotones: function(selector){
+		var x = document.querySelectorAll(selector);
+		for (var i = 0; i<x.length;i++) {
+			x[i].onmouseover = this.eventoAchicaBoton;
+			x[i].onmouseleave = this.eventoVuelveBoton;
+		};
+	},
 
-window.onload = function(){ //Acciones tras cargar la página
-pantalla=document.getElementById("display"); //elemento pantalla de salida
-} //Sirve para poder escribir en pantalla;
+	eventoAchicaBoton: function(event){
+		calculadora.AchicaBoton(event.target);
+	},
 
+	eventoVuelveBoton: function(event){
+		calculadora.AumentaBoton(event.target);
+	},
+	
+	//Formato de botones 
+	
+	AchicaBoton: function(elemento){
+		var x = elemento.id;
+		if (x=="1" || x=="2" || x=="3" || x=="0" || x=="igual" || x=="punto" ) {
+			elemento.style.width = "28%";
+			elemento.style.height = "62px";
+		} else if(x=="mas") {
+			elemento.style.width = "88%";
+			elemento.style.height = "98%";
+		} else {
+		elemento.style.width = "21%";
+		elemento.style.height = "62px";
+		}
+	},
+	
+	AumentaBoton: function(elemento){
+		var x = elemento.id;
+		if (x=="1" || x=="2" || x=="3" || x=="0" || x=="igual" || x=="punto" ) {
+			elemento.style.width = "29%";
+			elemento.style.height = "62.91px";
+		} else if(x=="mas") {
+			elemento.style.width = "90%";
+			elemento.style.height = "100%";
+		} else {
+		elemento.style.width = "22%";
+		elemento.style.height = "62.91px";
+		}
+	},
+	
+	asignarEventosaFuncion: function(){
+		document.getElementById("0").addEventListener("click", function() {calculadora.ingresoNumero("0");});
+		document.getElementById("1").addEventListener("click", function() {calculadora.ingresoNumero("1");});
+		document.getElementById("2").addEventListener("click", function() {calculadora.ingresoNumero("2");});
+		document.getElementById("3").addEventListener("click", function() {calculadora.ingresoNumero("3");});
+		document.getElementById("4").addEventListener("click", function() {calculadora.ingresoNumero("4");});
+		document.getElementById("5").addEventListener("click", function() {calculadora.ingresoNumero("5");});
+		document.getElementById("6").addEventListener("click", function() {calculadora.ingresoNumero("6");});
+		document.getElementById("7").addEventListener("click", function() {calculadora.ingresoNumero("7");});
+		document.getElementById("8").addEventListener("click", function() {calculadora.ingresoNumero("8");});
+		document.getElementById("9").addEventListener("click", function() {calculadora.ingresoNumero("9");});
+		document.getElementById("on").addEventListener("click", function() {calculadora.borrarVisor();});
+		document.getElementById("sign").addEventListener("click", function() {calculadora.cambiarSigno();});
+		document.getElementById("punto").addEventListener("click", function() {calculadora.ingresoDecimal();});
+		document.getElementById("igual").addEventListener("click", function() {calculadora.verResultado();});
+		document.getElementById("raiz").addEventListener("click", function() {calculadora.ingresoOperacion("raiz");});
+		document.getElementById("dividido").addEventListener("click", function() {calculadora.ingresoOperacion("/");});
+		document.getElementById("por").addEventListener("click", function() {calculadora.ingresoOperacion("*");});
+		document.getElementById("menos").addEventListener("click", function() {calculadora.ingresoOperacion("-");});
+		document.getElementById("mas").addEventListener("click", function() {calculadora.ingresoOperacion("+");});
+	},
+	
+	borrarVisor: function(){ 
 
+	    this.valorVisor = "0";
+		this.operacion = "";
+		this.primerValor = 0;
+		this.segundoValor = 0;
+		this.resultado = 0;
+		this.Operación = "";
+		this.auxTeclaIgual = false;
+		this.ultimoValor = 0;
+		this.updateVisor();
+	},
+	
+	cambiarSigno: function(){
+		if (this.valorVisor !="0") {
+			var aux;
+			if (this.valorVisor.charAt(0)=="-") {
+				aux = this.valorVisor.slice(1);
+			}	else {
+				aux = "-" + this.valorVisor;
+			}
+		this.valorVisor = "";
+		this.valorVisor = aux;
+		this.updateVisor();
+		}
+	},
+	
+	ingresoDecimal: function(){
+		if (this.valorVisor.indexOf(".")== -1) {
+			if (this.valorVisor == ""){
+				this.valorVisor = this.valorVisor + "0.";
+			} else {
+				this.valorVisor = this.valorVisor + ".";
+			}
+			this.updateVisor();
+		}
+	},
+	
+	ingresoNumero: function(valor){
+		if (this.valorVisor.length < 8) {
+		
+			if (this.valorVisor=="0") {
+				this.valorVisor = "";
+				this.valorVisor = this.valorVisor + valor;
+			} else {
+				this.valorVisor = this.valorVisor + valor;
+			}
+		this.updateVisor();
+		}
+	},
+	
+	ingresoOperacion: function(oper){
+		this.primerValor = parseFloat(this.valorVisor);
+		this.valorVisor = "";
+		this.operacion = oper;
+		this.auxTeclaIgual = false;
+		this.updateVisor();
+	},
+	
+	verResultado: function(){
 
-var x="0"; //guardar número en pantalla
-var xi=1; //iniciar número en pantalla: 1=si; 0=no;
-var coma = 0; //estado coma decimal 0=no, 1=si;
-var ni = 0;
-var op = "no";
-var total;
+		if(!this.auxTeclaIgual){ 
+			this.segundoValor = parseFloat(this.valorVisor);
+			this.ultimoValor = this.segundoValor;
+			this.realizarOperacion(this.primerValor, this.segundoValor, this.operacion);
+		
+		} else {
+			this.realizarOperacion(this.primerValor, this.ultimoValor, this.operacion);
+		}
+	
+		this.primerValor = this.resultado;
+		this.valorVisor = "";
+	
+		if (this.resultado.toString().length < 9){
+			this.valorVisor = this.resultado.toString();
+		} else {
+			this.valorVisor = this.resultado.toString().slice(0,8) + "...";
+		}
+	
+		this.auxTeclaIgual = true;		
+		this.updateVisor();
+	
+	},
+	
+	realizarOperacion: function(primerValor, segundoValor, operacion){
+		switch(operacion){
+			case "+": 
+				this.resultado = eval(primerValor + segundoValor);
+			break;
+			case "-": 
+				this.resultado = eval(primerValor - segundoValor);
+			break;
+			case "*": 
+				this.resultado = eval(primerValor * segundoValor);
+			break;
+			case "/": 
+				this.resultado = eval(primerValor / segundoValor);
+			break;
+			case "raiz":
+				this.resultado = eval(Math.sqrt(primerValor));
+		}
+	},
+	
+	updateVisor: function(){
+		this.visor.innerHTML = this.valorVisor;
+	}
+	
+};
 
-function numero(xx) {  //recoge el número pulsado en el argumento.
-         if (x=="0" || xi==1  ) {  // inicializar un número
-            display.innerHTML=xx; //mostrar en pantalla
-            x=xx; //guardar número;
-            if (xx==".") { //si escribimos una coma al principio del número
-               display.innerHTML="0."; //escribimos 0.
-               x=xx; //guardar número
-               coma=1; //cambiar estado de la coma
-             }
-           }
-         else { //continuar un número
-           if (xx=="." && coma == 0){//si escribimos una coma decimal pòr primera vez
-             display.innerHTML+=xx; //añadimos y mostramos en pantalla.
-             x+=xx; //añadimos y guardamos
-             coma = 1; //cambio valor de coma para no poder agregar mas
-           }
-            else if (xx=="." && coma==1) {}//si intentamos escribir una segunda coma decimal no realiza ninguna acción.
-            else {
-              if(display.innerHTML.length <= 7){
-              display.innerHTML+=xx;
-            }
-              x+=xx;
-            }
-            }
-         xi=0; //el número está iniciado y podemos ampliarlo.
-     }
-
-function operar(s) {
-    igualar();
-    ni=x //ponemos el 1º número en "numero en espera" para poder escribir el segundo.
-    op=s; //guardamos tipo de operación.
-    display.innerHTML=op;
-    xi=1; //inicializar pantalla.
-  }
-
-  function igualar() {
-         if (op=="no") { //no hay ninguna operación pendiente.
-            display.innerHTML=x;	//mostramos el mismo número
-            }
-         else { //con operación pendiente resolvemos
-            sl=ni+op+x; // escribimos la operación en una cadena
-            sol=eval(sl) //convertimos la cadena a código y resolvemos
-            total = String(sol);
-            total1= total.substring(0,8);
-            display.innerHTML=total1 //mostramos la solución
-            x=sol; //guardamos la solución
-            op="no"; //ya no hay operaciones pendientes
-            xi=1; //se puede reiniciar la pantalla.
-            }
-        }
-
-function opuest() {
-  nx=Number(x); //convertir en número
-  nx=-nx; //cambiar de signo
-  x=String(nx); //volver a convertir a cadena
-  t=x.substring(0,8);
-  display.innerHTML=t; //mostrar en pantalla.
-}
-
-function raiz() {
-         x=Math.sqrt(x) //resolver raíz cuadrada.
-         display.innerHTML=x; //mostrar en pantalla resultado
-         op="no"; //quitar operaciones pendientes.
-         xi=1; //se puede reiniciar la pantalla
-         }
-
-function reiniciar() {
-         pantalla.innerHTML=0; //poner pantalla a 0
-         x="0"; //reiniciar número en pantalla
-         coma=0; //reiniciar estado coma decimal
-         ni=0 //indicador de número oculto a 0;
-         op="no" //borrar operación en curso.
-         }
-
-/* Disminuir y aumentar botones*/
-function disminuir1(){
-  document.getElementById('1').style='width: 76px';
-}
-
-function aumentar1(){
-  document.getElementById('1').style='width: 80px';
-}
-
-function disminuir2(){
-  document.getElementById('2').style='width: 76px';
-}
-
-function aumentar2(){
-  document.getElementById('2').style='width: 80px';
-}
-
-function disminuir3(){
-  document.getElementById('3').style='width: 76px';
-}
-
-function aumentar3(){
-  document.getElementById('3').style='width: 80px';
-}
-
-function disminuir4(){
-  document.getElementById('4').style='width: 76px';
-}
-
-function aumentar4(){
-  document.getElementById('4').style='width: 80px';
-}
-
-function disminuir5(){
-  document.getElementById('5').style='width: 76px';
-}
-
-function aumentar5(){
-  document.getElementById('5').style='width: 80px';
-}
-
-function disminuir6(){
-  document.getElementById('6').style='width: 76px';
-}
-
-function aumentar6(){
-  document.getElementById('6').style='width: 80px';
-}
-
-function disminuir7(){
-  document.getElementById('7').style='width: 76px';
-}
-
-function aumentar7(){
-  document.getElementById('7').style='width: 80px';
-}
-
-function disminuir8(){
-  document.getElementById('8').style='width: 76px';
-}
-
-function aumentar8(){
-  document.getElementById('8').style='width: 80px';
-}
-
-function disminuir9(){
-  document.getElementById('9').style='width: 76px';
-}
-
-function aumentar9(){
-  document.getElementById('9').style='width: 80px';
-}
-
-function disminuir0(){
-  document.getElementById('0').style='width: 76px';
-}
-
-function aumentar0(){
-  document.getElementById('0').style='width: 80px';
-}
-
-function disminuirmas(){
-  document.getElementById('mas').style='width: 78px';
-}
-
-function aumentarmas(){
-  document.getElementById('mas').style='width: 80px';
-}
-
-function disminuirmenos(){
-  document.getElementById('menos').style='width: 76px';
-}
-
-function aumentarmenos(){
-  document.getElementById('menos').style='width: 80px';
-}
-
-function disminuirpor(){
-  document.getElementById('por').style='width: 76px';
-}
-
-function aumentarpor(){
-  document.getElementById('por').style='width: 80px';
-}
-
-function disminuirdividir(){
-  document.getElementById('dividido').style='width: 76px';
-}
-
-function aumentardividir(){
-  document.getElementById('dividido').style='width: 80px';
-}
-
-function disminuirpunto(){
-  document.getElementById('punto').style='width: 76px';
-}
-
-function aumentarpunto(){
-  document.getElementById('punto').style='width: 80px';
-}
-
-function disminuirigual(){
-  document.getElementById('igual').style='width: 76px';
-}
-
-function aumentarigual(){
-  document.getElementById('igual').style='width: 80px';
-}
-
-function disminuiron(){
-  document.getElementById('on').style='width: 76px';
-}
-
-function aumentaron(){
-  document.getElementById('on').style='width: 80px';
-}
-
-function disminuirsigno(){
-  document.getElementById('sign').style='width: 76px';
-}
-
-function aumentarsigno(){
-  document.getElementById('sign').style='width: 80px';
-}
-
-function disminuirraiz(){
-  document.getElementById('raiz').style='width: 76px';
-}
-
-function aumentarraiz(){
-  document.getElementById('raiz').style='width: 80px';
-}
-
-document.getElementById('raiz').onmousedown=disminuirraiz;
-document.getElementById('raiz').onmouseup=aumentarraiz;
-document.getElementById('sign').onmousedown=disminuirsigno;
-document.getElementById('sign').onmouseup=aumentarsigno;
-document.getElementById('1').onmousedown=disminuir1;
-document.getElementById('1').onmouseup=aumentar1;
-document.getElementById('2').onmouseodown=disminuir2;
-document.getElementById('2').onmouseup=aumentar2;
-document.getElementById('3').onmousedown=disminuir3;
-document.getElementById('3').onmouseup=aumentar3;
-document.getElementById('4').onmousedown=disminuir4;
-document.getElementById('4').onmouseup=aumentar4;
-document.getElementById('5').onmousedown=disminuir5;
-document.getElementById('5').onmouseup=aumentar5;
-document.getElementById('6').onmousedown=disminuir6;
-document.getElementById('6').onmouseup=aumentar6;
-document.getElementById('7').onmousedown=disminuir7;
-document.getElementById('7').onmouseup=aumentar7;
-document.getElementById('8').onmousedown=disminuir8;
-document.getElementById('8').onmouseup=aumentar8;
-document.getElementById('9').onmousedown=disminuir9;
-document.getElementById('9').onmouseup=aumentar9;
-document.getElementById('0').onmousedown=disminuir0;
-document.getElementById('0').onmouseup=aumentar0;
-document.getElementById('mas').onmousedown=disminuirmas;
-document.getElementById('mas').onmouseup=aumentarmas;
-document.getElementById('menos').onmousedown=disminuirmenos;
-document.getElementById('menos').onmouseup=aumentarmenos;
-document.getElementById('dividido').onmousedown=disminuirdividir;
-document.getElementById('dividido').onmouseup=aumentardividir;
-document.getElementById('por').onmousedown=disminuirpor;
-document.getElementById('por').onmouseup=aumentarpor;
-document.getElementById('punto').onmousedown=disminuirpunto;
-document.getElementById('punto').onmouseup=aumentarpunto;
-document.getElementById('on').onmousedown=disminuiron;
-document.getElementById('on').onmouseup=aumentaron;
-document.getElementById('igual').onmousedown=disminuirigual;
-document.getElementById('igual').onmouseup=aumentarigual;
+calculadora.init();
